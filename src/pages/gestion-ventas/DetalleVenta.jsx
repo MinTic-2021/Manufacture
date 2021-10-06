7e9f1542c57987bc6d793b85c3f109a3e7449c15
Karen Ramirez Moreno <kdramirezm@gmail.com>
2021-10-05 22:38:24 -0500

    DetalleVenta y GestionVenta

--- src/pages/gestion-ventas/DetalleVenta.jsx
+++ src/pages/gestion-ventas/DetalleVenta.jsx
@@ -3,9 +3,82 @@ import React from 'react'
 const DetalleVenta = () => {
     return (
         <div>
-            
+            <div className="container-sm">
+            <h4 style={{paddingBottom: '20px', paddingTop: '24px', display: 'flex', justifyContent: 'center'}}>Detalle de venta</h4>
+                <ul className="list-unstyled">
+                    <li><b>Identificador de venta:</b> <span>DF001</span></li>
+                    <li><b>Fecha de venta:</b> <span>02/10/2021</span></li>
+                    <li><b>Id cliente:</b> <span>85093573</span></li>
+                    <li><b>Nombre cliente:</b> <span>Edgardo Cadavid Machado</span></li>
+                    <li><b>Id vendedor:</b> <span>1110765789</span></li>
+                </ul>  
+                <div>
+                    <table className="table table-hover" style={{paddingLeft: '50px'}}>
+                    <thead>
+                    <tr>
+                        <th width='8%'>ID</th>
+                        <th width='25%'>Descripción</th>
+                        <th width='12%'>Cantidad</th>
+                        <th width='15%'>Valor unitario</th>
+                        <th width='15%'>Total</th>
+                    </tr>
+                    </thead>
+                    <tbody style={{height: '30vh'}}>
+                        <tr>
+                            <td contenteditable="true"> </td>
+                            <td> </td>
+                            <td contenteditable="true"> </td>
+                            <td> </td>
+                            <td> </td>
+                        </tr>
+                        <tr>
+                            <td contenteditable="true"> </td>
+                            <td> </td>
+                            <td contenteditable="true"> </td>
+                            <td> </td>
+                            <td> </td>
+                        </tr>
+                        <tr>
+                            <td contenteditable="true"> </td>
+                            <td> </td>
+                            <td contenteditable="true"> </td>
+                            <td> </td>
+                            <td> </td>
+                        </tr>
+                        <tr>
+                            <td contenteditable="true"> </td>
+                            <td> </td>
+                            <td contenteditable="true"> </td>
+                            <td> </td>
+                            <td> </td>
+                        </tr>
+                        <tr style={{borderBottomColor: 'black'}}>
+                            <td contenteditable="true"> </td>
+                            <td> </td>
+                            <td contenteditable="true"> </td>
+                            <td> </td>
+                            <td> </td>
+                        </tr>
+                    </tbody>
+                    </table>
+                </div>
+                Total de la venta: <input type="text" disabled/>
+                <div style={{paddingTop: '12px'}}>
+                    <button type="button" className="btn btn-secondary" onclick="alert('Venta actualizada')" style={{paddingTop: '0.8px', paddingBottom: '1px'}}>
+                        Modificar
+                    </button>
+                    <button type="button" className="btn btn-secondary" onclick="alert('Venta eliminada')"style={{paddingTop: '0.8px', paddingBottom: '1px'}}>
+                        Eliminar
+                    </button>
+                    <button type="button" className="btn btn-secondary" style={{paddingTop: '0.8px', paddingBottom: '1px'}}>
+                        Cancelar
+                    </button>
+                </div>
+            </div>            
+
         </div>
     )
 }
 
-export default DetalleVenta
+export default DetalleVenta;
+
--- src/pages/gestion-ventas/GestionVenta.jsx
+++ src/pages/gestion-ventas/GestionVenta.jsx
@@ -2,10 +2,167 @@ import React from 'react'
 
 const GestionVenta = () => {
     return (
-        <div>
-            
-        </div>
+        <div style={{paddingBottom: '40px'}}>
+            <div className="container-sm">
+                <h4 style={{paddingBottom: '20px', paddingTop: '20px', display: 'flex', justifyContent: 'center'}}>
+                    Gestión de ventas
+                </h4>
+                <h6>Buscar venta:</h6>
+                <div style={{display: 'flex', justifyContent: 'left', alignItems: 'center'}}>
+                    <select className="form-select form-select-sm" style={{width: '13%'}}>
+                        <option value="id">ID venta</option>
+                        <option value="producto">ID Cliente</option>
+                        <option value="vendedor">Nombre cliente</option>
+                        <option value="todo">Mostrar todo</option>
+                    </select> 
+                    <div style={{paddingRight: '12px', paddingLeft: '12px'}}>     
+                        <input type="text"/>
+                    </div>   
+                    <button type="button" className="btn btn-secondary" style={{paddingTop: '0.8px', paddingBottom: '1px'}}>
+                        Buscar
+                    </button>
+                </div>                
+                    <div style={{paddingTop: '20px'}}>
+                    <table className="table table-hover" style={{paddingLeft: '50px'}}>
+                        <thead>
+                        <tr>
+                            <th>ID venta</th>
+                            <th>Valor total</th>
+                            <th>Fecha venta</th>
+                            <th>ID cliente</th>
+                            <th>Nombre cliente</th>
+                            <th>ID vendedor</th>
+                            <th>Detalle venta</th>
+                        </tr>
+                        </thead>
+                        <tbody style={{height: '30vh'}}>
+                            <tr style={{height: '5vh'}}>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                                <td style={{paddingTop: '5px', paddingBottom: '0%'}}>
+                                    <div style={{paddingLeft: '40px', cursor: 'pointer'}}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-up-right-square" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z"/>
+                                        </svg>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr style={{height: '5vh'}}>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                                <td style={{paddingTop: '5px', paddingBottom: '0%'}}>
+                                    <div style={{paddingLeft: '40px', cursor: 'pointer'}}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-up-right-square" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z"/>
+                                        </svg>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr style={{height: '5vh'}}>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                                <td style={{paddingTop: '5px', paddingBottom: '0%'}}>
+                                    <div style={{paddingLeft: '40px', cursor: 'pointer'}}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-up-right-square" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z"/>
+                                        </svg>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr style={{height: '5vh'}}>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
+                                <td style={{paddingTop: '5px', paddingBottom: '0%'}}>
+                                    <div style={{paddingLeft: '40px', cursor: 'pointer'}}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-up-right-square" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z"/>
+                                        </svg>
+                                    </div>
+                                </td>
+                            </tr>
+                                <tr style={{borderBottomColor: 'black', height: '5vh'}}>
+                                    <td> </td>
+                                    <td> </td>
+                                    <td> </td>
+                                    <td> </td>
+                                    <td> </td>
+                                    <td> </td>
+                                    <td style={{paddingTop: '5px', paddingBottom: '0%'}}>
+                                        <div style={{paddingLeft: '40px', cursor: 'pointer'}}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-up-right-square" viewBox="0 0 16 16">
+                                            <defaultpath fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z"/>
+                                            </svg>
+                                        </div>
+                                    </td>
+                                </tr>
+                        </tbody>
+                    </table>
+                    </div>
+                </div>
+                <h4 style={{paddingBottom: '20px', paddingTop: '20px', display: 'flex', justifyContent: 'center'}}>
+                    Estado de ventas
+                </h4>
+                <h6>Buscar pedido:</h6>
+                <div style={{display: 'flex', justifyContent: 'left', alignItems: 'center'}}>
+                    <span>ID venta</span>
+                    <div style={{paddingRight: '12px', paddingLeft: '12px'}}>     
+                    <input type="text"/>
+                    </div>   
+                    <button type="button" className="btn btn-secondary" style={{paddingTop: '0.8px', paddingBottom: '1px'}}>
+                        Buscar
+                    </button>
+                </div>
+                <div style={{paddingTop: '20px'}}>
+                    <table className="table table-hover" style={{paddingLeft: '50px'}}>
+                        <thead>
+                        <tr>
+                            <th>ID venta</th>
+                            <th>Descripción venta</th>
+                            <th>Estado</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            <tr style={{borderBottomColor: 'black'}}>
+                                <td width= '15%'> </td>
+                                    <td> </td>
+                                    <td width='20%' style={{paddingTop: '0%', paddingBottom: '0%', paddingRight: '0%'}}>
+                                        <select className="form-select form-select-sm" style={{width: '100%', borderColor: 'rgba(255, 255, 255, 0)'}}>
+                                            <option value="id">En proceso</option>
+                                            <option value="producto">Cancelada</option>
+                                            <option value="vendedor">Entregada</option>
+                                        </select>  
+                                    </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div style={{paddingTop: '12px', paddingBottom: '60px', display: 'flex', justifyContent: 'left'}}>
+                    <button type="button" className="btn btn-secondary" onclick="alert('Información actualizada')" style={{paddingTop: '0px', paddingBottom: '1px'}}>
+                        Actualizar Estado
+                    </button>
+                </div>
+
+            </div>
+        
     )
 }
 
-export default GestionVenta
+export default GestionVenta;
+
+
