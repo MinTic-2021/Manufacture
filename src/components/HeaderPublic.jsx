152ecf5ef04010b3ac2d063e2a153e2f5d8fe31c
jsmendozap <jsmendozap@unal.edu.co>
2021-10-10 12:47:04 -0500

    corrección navbar

--- src/components/HeaderPublic.jsx
+++ src/components/HeaderPublic.jsx
@@ -4,14 +4,14 @@ import devfull from 'media/Devfull.png'
 const HeaderPublic = () => {
     return (
         <header>
-            <nav class="navbar navbar-expand-lg navbar-light" style={{backgroundColor: "#f1f1f1"}}>
-            <div class="container-fluid" style={{display:"grid", gridTemplateColumns: "10% 80%"}}>
+            <nav className="navbar navbar-expand-lg navbar-light" style={{backgroundColor: "#f1f1f1"}}>
+            <div className="container-fluid" style={{display:"grid", gridTemplateColumns: "10% 80%"}}>
             <img src={devfull} style={{height: "35px", paddingRight: "20px"}} alt="logo"/>
-                    <h3 class="navbar-brand" style={{marginLeft: "27vh"}}>
+                    <h3 className="navbar-brand" style={{marginLeft: "27vh"}}>
                         Plataforma de Gestión de Ventas e Inventario
                     </h3>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
                 </div>                            
             </nav>
